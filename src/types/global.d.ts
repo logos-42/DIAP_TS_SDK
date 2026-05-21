4fdf594799e62e7bb58b59eb71a1f6694168662f
yuanjie liu <2844169590@qq.com>
2026-05-21 11:44:23 +0800

    feat(types): add libp2p and multiformats type definitions
    Add ambient module declarations for libp2p, @libp2p/*, and @multiformats/*
    to support TypeScript integration for the newly implemented p2p features.

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -4,6 +4,138 @@
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+declare module '@libp2p/interface-peer-id' {
+  export interface PeerId {
+    toString(): string;
+    toBytes(): Uint8Array;
+  }
+}
+
+declare module '@libp2p/interface-connection' {
+  export interface Connection {
+    remotePeer(): PeerId;
+    remoteAddr(): Multiaddr;
+    close(): Promise<void>;
+  }
+  export interface Multiaddr {
+    toString(): string;
+  }
+  export interface PeerId {
+    toString(): string;
+    toBytes(): Uint8Array;
+  }
+}
+
+declare module '@libp2p/interface-pubsub' {
+  export interface PubSub {
+    start(): Promise<void>;
+    stop(): Promise<void>;
+    subscribe(topic: string): void;
+    unsubscribe(topic: string): void;
+    publish(topic: string, data: Uint8Array): Promise<void>;
+    on(event: string, handler: (topic: string, msg: any) => void): void;
+    addEventListener(event: string, handler: (evt: any) => void): void;
+    removeEventListener(event: string, handler: (evt: any) => void): void;
+  }
+}
+
+declare module '@chainsafe/libp2p-gossipsub' {
+  export interface Gossipsub extends PubSub {
+    addPeer(peerId: any, protocol: string): void;
+    removePeer(peerId: any): void;
+  }
+  export function gossipsub(init?: any): Gossipsub;
+  export interface PubSub {
+    start(): Promise<void>;
+    stop(): Promise<void>;
+    subscribe(topic: string): void;
+    publish(topic: string, data: Uint8Array): Promise<void>;
+    on(event: string, handler: (topic: string, msg: any) => void): void;
+  }
+}
+
+declare module 'libp2p' {
+  export interface Libp2pOptions {
+    peerId?: any;
+    addresses?: any;
+    connectionManager?: any;
+    transports?: any[];
+    streamMuxers?: any[];
+    connectionEncryption?: any[];
+    datastore?: any;
+    pubsub?: any;
+    services?: any;
+    peerDiscovery?: any[];
+    [key: string]: any;
+  }
+  export class Libp2p {
+    constructor(options?: Libp2pOptions);
+    start(): Promise<void>;
+    stop(): Promise<void>;
+    dial(peer: any): Promise<any>;
+    hangUp(peer: any): Promise<void>;
+    peerStore: any;
+    multiaddrs: any;
+    getPeers(): any[];
+    isStarted(): boolean;
+    services?: any;
+    peerId?: any;
+    addEventListener(event: string, handler: any): void;
+    removeEventListener(event: string, handler: any): void;
+    getMultiaddrs(): any;
+  }
+  export function createLibp2p(options?: Libp2pOptions): Promise<Libp2p>;
+}
+
+declare module '@multiformats/multiaddr' {
+  export class Multiaddr {
+    constructor(addr: string | Uint8Array);
+    toString(): string;
+    toBytes(): Uint8Array;
+    toArray(): Multiaddr[];
+    static fromString(addr: string): Multiaddr;
+  }
+  export function multiaddr(addr: string | Uint8Array): Multiaddr;
+}
+
+declare module '@libp2p/tcp' {
+  export function tcp(): any;
+}
+
+declare module '@libp2p/websockets' {
+  export function webSockets(): any;
+}
+
+declare module '@libp2p/webrtc-star' {
+  export function webRTCStar(): any;
+}
+
+declare module '@libp2p/mplex' {
+  const mplex: any;
+  export = mplex;
+}
+
+declare module '@libp2p/noise' {
+  const noise: any;
+  export = noise;
+}
+
+declare module '@libp2p/identify' {
+  export function identify(): any;
+}
+
+declare module '@libp2p/gater' {
+  export function gater(options?: any): any;
+}
+
+declare module '@libp2p/mdns' {
+  export function mdns(options?: any): any;
+}
+
+declare module '@libp2p/bootstrap' {
+  export function bootstrap(options?: any): any;
+}
+
 // 声明 @noble/ed25519 模块
 declare module '@noble/ed25519' {
   export function getPublicKey(privateKey: Uint8Array): Uint8Array;
@@ -28,7 +160,9 @@ declare module '@noble/ed25519' {
 // 声明 @noble/hashes 模块
 declare module '@noble/hashes' {
   export * from './sha256.js';
+  export * from './sha2.js';
   export * from './blake2.js';
+  export * from './blake2s.js';
 }
 
 declare module '@noble/hashes/sha256' {
@@ -36,8 +170,25 @@ declare module '@noble/hashes/sha256' {
   export function sha512(message: Uint8Array | string): Uint8Array;
 }
 
+declare module '@noble/hashes/sha2' {
+  export function sha256(message: Uint8Array | string): Uint8Array;
+  export function sha512(message: Uint8Array | string): Uint8Array;
+}
+
 declare module '@noble/hashes/blake2' {
-  export function blake2b(message: Uint8Array | string, outputLength?: number): Uint8Array;
+  export function blake2b(message: Uint8Array | string, outputLength?: number | { dkLen?: number }): Uint8Array;
+  export function blake2s(message: Uint8Array | string, outputLength?: number | { dkLen?: number }): Uint8Array;
+}
+
+declare module '@noble/hashes/blake2.js' {
+  export * from '@noble/hashes/blake2';
+}
+
+declare module '@noble/hashes/sha2.js' {
+  export * from '@noble/hashes/sha2';
+}
+
+declare module '@noble/hashes/blake2s' {
   export function blake2s(message: Uint8Array | string, outputLength?: number): Uint8Array;
 }
 
@@ -51,10 +202,16 @@ declare module 'bs58' {
 declare module '@iroh-js/client' {
   export class Endpoint {
     static builder(): EndpointBuilder;
-    bind(): Promise<Endpoint>;
+    nodeId(): Promise<{ toString(): string }>;
+    connect(peerId: any, addr?: string): Promise<any>;
+    close(): Promise<void>;
+    onIncomingConnection(handler: (conn: any) => void): void;
   }
   export class EndpointBuilder {
     bind(): Promise<Endpoint>;
+    dataDir(dir: string): this;
+    addRelay(relay: string): this;
+    enableNatTraversal(): this;
   }
 }
 
